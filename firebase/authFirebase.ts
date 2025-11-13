f9c3fdb229315f22b17fcbe24d4a6600ee3c3c01
Arseny <arseny@MacBook-Pro-Arseny.local>
2025-11-13 09:59:35 +0300

    mock register

--- firebase/authFirebase.ts
+++ firebase/authFirebase.ts
@@ -7,7 +7,7 @@ import app from '../firebaseConfig'
 
 const auth = getAuth(app)
 
-export const registerUser = (email = 'qqq@www.ee', password = '123321') => {
+export const registerUser = (email = 'qqq@www2.ee', password = '123321') => {
   return createUserWithEmailAndPassword(auth, email, password).then(
     (userCredential) => {
       return userCredential.user.getIdToken()
